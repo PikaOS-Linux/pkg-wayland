8d3799acade33fab1113a0a223f6177cfbbbf29c
ferrreo <harderthanfire@gmail.com>
2023-06-10 18:56:46 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ dpkg-sig --sign builder ./output/*.deb
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
-reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libwayland-doc*_all*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% libwayland-doc*)'
 
 # Add the new package to the repo
 reprepro -V --basedir ./output/repo/ includedeb lunar ./output/*.deb
