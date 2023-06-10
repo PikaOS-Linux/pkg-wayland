b8a54d0ec03cf501a18b184cb2df7b5c9f20ffa8
ferrreo <harderthanfire@gmail.com>
2023-06-10 18:59:01 +0100

    32bit build fix docs

--- main32.sh
+++ main32.sh
@@ -11,6 +11,7 @@ wget -nv https://gitlab.freedesktop.org/wayland/wayland/-/archive/1.22.0/wayland
 tar -xf ./wayland-1.22.0.tar.gz
 mv ./wayland-1.22.0.tar.gz ./wayland_1.22.0.orig.tar.gz
 cp -rvf ./debian ./wayland-1.22.0/
+cp -vf ./control32 ./wayland-1.22.0/debian/control
 cd ./wayland-1.22.0/
 
 # Get build deps
