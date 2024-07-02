db7f1abb5cbc069316c852b95ba4c271232bdde9
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-07-02 03:12:29 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,10 +4,10 @@ DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
 
-wget -nv https://gitlab.freedesktop.org/wayland/wayland/-/archive/1.23.0/wayland-1.23.0.tar.gz
-tar -xf ./wayland-1.23.0.tar.gz
-cp -rvf ./debian ./wayland-1.23.0/
-cd ./wayland-1.23.0/
+wget -nv https://gitlab.freedesktop.org/wayland/wayland/-/archive/1.22.0/wayland-1.22.0.tar.gz
+tar -xf ./wayland-1.22.0.tar.gz
+cp -rvf ./debian ./wayland-1.22.0/
+cd ./wayland-1.22.0/
 
 # Get build deps
 apt-get build-dep ./ -y
