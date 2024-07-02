2975c71118a3b1c5a9db51eb44fc2e89fa6d8133
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-07-02 03:13:23 +0300

    Update main32.sh

--- main32.sh
+++ main32.sh
@@ -4,12 +4,11 @@ DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
 
-wget -nv https://gitlab.freedesktop.org/wayland/wayland/-/archive/1.23.0/wayland-1.23.0.tar.gz
-tar -xf ./wayland-1.23.0.tar.gz
-mv ./wayland-1.23.0.tar.gz ./wayland_1.23.0.orig.tar.gz
-cp -rvf ./debian ./wayland-1.23.0/
-cp -vf ./control32 ./wayland-1.23.0/debian/control
-cd ./wayland-1.23.0/
+wget -nv https://gitlab.freedesktop.org/wayland/wayland/-/archive/1.22.0/wayland-1.22.0.tar.gz
+tar -xf ./wayland-1.22.0.tar.gz
+cp -rvf ./debian ./wayland-1.22.0/
+cp -vf ./control32 ./wayland-1.22.0/debian/control
+cd ./wayland-1.22.0/
 
 # Get build deps
 apt-get build-dep ./ -y
